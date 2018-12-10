ce61d4675cd6636f5ad00bbac5c506d0be4f529b
Jasper Flick <jrjflick@catlikecoding.com>
2018-12-10 15:40:31 +0100

    Initial Commit.

---
+++ Assets/My Pipeline/MyRP/Editor/MyShaderIncludePaths.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+using UnityEditor;
+
+static class MyShaderIncludePaths {
+
+	[ShaderIncludePath]
+	static string[] GetPaths () {
+		return new string[] {
+			Path.GetFullPath("Assets/My Pipeline")
+		};
+	}
+}
