e366b767c51e325630cf7d88b338e421051c8038
Jasper Flick <support@catlikecoding.com>
2020-11-11 15:03:28 +0100

    Fix WebGL 2.0 incompatible shadow sampler error when no shadows are rendered.

--- Assets/Custom RP/Runtime/Shadows.cs
+++ Assets/Custom RP/Runtime/Shadows.cs
@@ -74,10 +74,8 @@ public class Shadows {
 	}
 
 	public void Cleanup () {
-		if (shadowedDirLightCount > 0) {
-			buffer.ReleaseTemporaryRT(dirShadowAtlasId);
-			ExecuteBuffer();
-		}
+		buffer.ReleaseTemporaryRT(dirShadowAtlasId);
+		ExecuteBuffer();
 	}
 
 	public Vector4 ReserveDirectionalShadows (
@@ -139,6 +137,12 @@ public class Shadows {
 	public void Render () {
 		if (shadowedDirLightCount > 0) {
 			RenderDirectionalShadows();
+		}
+		else {
+			buffer.GetTemporaryRT(
+				dirShadowAtlasId, 1, 1,
+				32, FilterMode.Bilinear, RenderTextureFormat.Shadowmap
+			);
 		}
 		buffer.BeginSample(bufferName);
 		SetKeywords(shadowMaskKeywords, useShadowMask ?
