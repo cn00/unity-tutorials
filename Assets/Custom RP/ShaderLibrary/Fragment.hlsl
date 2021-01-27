86a423d2284b7e1045f7b78a76b7423d4f7751de
Jasper Flick <support@catlikecoding.com>
2021-01-27 14:24:30 +0100

    Sample explicit LOD 0 of buffers.

--- Assets/Custom RP/ShaderLibrary/Fragment.hlsl
+++ Assets/Custom RP/ShaderLibrary/Fragment.hlsl
@@ -18,7 +18,7 @@ Fragment GetFragment (float4 positionSS) {
 	f.depth = IsOrthographicCamera() ?
 		OrthographicDepthBufferToLinear(positionSS.z) : positionSS.w;
 	f.bufferDepth =
-		SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, sampler_point_clamp, f.screenUV);
+		SAMPLE_DEPTH_TEXTURE_LOD(_CameraDepthTexture, sampler_point_clamp, f.screenUV, 0);
 	f.bufferDepth = IsOrthographicCamera() ?
 		OrthographicDepthBufferToLinear(f.bufferDepth) :
 		LinearEyeDepth(f.bufferDepth, _ZBufferParams);
@@ -27,7 +27,7 @@ Fragment GetFragment (float4 positionSS) {
 
 float4 GetBufferColor (Fragment fragment, float2 uvOffset = float2(0.0, 0.0)) {
 	float2 uv = fragment.screenUV + uvOffset;
-	return SAMPLE_TEXTURE2D(_CameraColorTexture, sampler_linear_clamp, uv);
+	return SAMPLE_TEXTURE2D_LOD(_CameraColorTexture, sampler_linear_clamp, uv, 0);
 }
 
 #endif
