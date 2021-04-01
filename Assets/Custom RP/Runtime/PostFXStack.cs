2aae4bfdaaac73c6258968c68b7943515b3ecc3a
Jasper Flick <support@catlikecoding.com>
2021-04-01 12:19:22 +0200

    Fixed field name typo.

--- Assets/Custom RP/Runtime/PostFXStack.cs
+++ Assets/Custom RP/Runtime/PostFXStack.cs
@@ -26,7 +26,7 @@ public partial class PostFXStack {
 	const int maxBloomPyramidLevels = 16;
 
 	int
-		bloomBucibicUpsamplingId = Shader.PropertyToID("_BloomBicubicUpsampling"),
+		bloomBicubicUpsamplingId = Shader.PropertyToID("_BloomBicubicUpsampling"),
 		bloomIntensityId = Shader.PropertyToID("_BloomIntensity"),
 		bloomPrefilterId = Shader.PropertyToID("_BloomPrefilter"),
 		bloomResultId = Shader.PropertyToID("_BloomResult"),
@@ -181,7 +181,7 @@ public partial class PostFXStack {
 
 		buffer.ReleaseTemporaryRT(bloomPrefilterId);
 		buffer.SetGlobalFloat(
-			bloomBucibicUpsamplingId, bloom.bicubicUpsampling ? 1f : 0f
+			bloomBicubicUpsamplingId, bloom.bicubicUpsampling ? 1f : 0f
 		);
 
 		Pass combinePass, finalPass;
